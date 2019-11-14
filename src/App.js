d4d16bc045e2cc16c7dea5a833833fb2f08bc761
Michael Harms <michael.harms6010@gmail.com>
2019-11-13 18:10:05 -0600

    created react app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+
 function App() {
+  let [boxClass, setBoxClass] = useState("greenBox")
+
+  const turnRed = _ => setBoxClass("redBox");
+  const turnGreen = _ => setBoxClass("greenBox")
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className={`box ${boxClass}`}>{boxClass}</div>
+      <button onClick={turnGreen}>Turn Green</button>
+      <button onClick={turnRed}>Turn Red</button>
     </div>
   );
 }
